99f3fc112cabbfb04021ddfc96b7160251ebecff
fconejer <fconejer@e9cd07b6-f61c-4518-8bde-cd466bdb5127>
2017-09-08 09:00:18 +0000

    rocci-client: added forgotten import.
    git-svn-id: http://margarita.bsc.es/svn/projects/conn-clients/trunk@1629 e9cd07b6-f61c-4518-8bde-cd466bdb5127

--- rocci-client/src/main/java/es/bsc/conn/clients/rocci/RocciClient.java
+++ rocci-client/src/main/java/es/bsc/conn/clients/rocci/RocciClient.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
